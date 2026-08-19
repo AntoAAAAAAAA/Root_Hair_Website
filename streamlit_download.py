bbc79d0d09a2e0a5f2fef4c1b3f76147086ff850
Anto <anto2005antony@gmail.com>
2026-08-19 17:34:57 -0500

    instructions in expander block

--- streamlit_download.py
+++ streamlit_download.py
@@ -4,19 +4,29 @@ st.title("Discovering Signals - Root Hair Analyzer")
 st.divider()
 
 st.text("Select your operating system from below.")
-
-st.text("Once the Google Drive link has opened, click 'Download', and save your .zip file to a safe location on your system.")
+# st.text("Once the Google Drive link has opened, click 'Download', and save your .zip file to a safe location on your system.")
 
 col1, col2 = st.columns(2)
 
 with col1.expander("MacOS", expanded=False):
-    st.link_button('Download App', "https://drive.google.com/file/d/1LFbcEDF2atjDAlBoKH3Mw-9CMhirEjDu/view?usp=drive_link")
-    st.text("Click 'Download App', and Google Drive link should open in a new tab.")
+    st.link_button('Download App', "https://drive.google.com/file/d/1LFbcEDF2atjDAlBoKH3Mw-9CMhirEjDu/view?usp=drive_link",
+                   type='primary')
+    st.text('')
+    st.text("1. Click 'Download App'")
+    st.text("2. A Google Drive link will open. Click 'Download'")
+    st.text("3. Save the .zip file to a safe location")
+    st.text("4. Open Finder, navigate to the .zip file, and double click it")
+    st.text("5. You should see a file called RootHairAnalyzer open; this is your app. Double click to open")
+    st.text("Optional: Move the app to your Applications folder for easier future access")
+
 with col2.expander("Windows", expanded=False):
-    st.link_button("Download App", "https://drive.google.com/file/d/1l5KtwsILhIPowhPa26pMESzytDKNlqkC/view?usp=drive_link")     
-
+    st.link_button("Download App", "https://drive.google.com/file/d/1l5KtwsILhIPowhPa26pMESzytDKNlqkC/view?usp=drive_link",
+                   type='primary')     
+    st.text('')
+    st.text("1. Click 'Download App'")
+    st.text("2. A Google Drive link will open. Click 'Download'")
+    st.text("3. Save the .zip file to a safe location")
+    st.text("4. Open Finder, navigate to the .zip file, and double click it")
+    st.text("5. You should see a file called RootHairAnalyzer open; this is your app. Double click to open")
+    st.text("Optional: Right click on the app and create a desktop shortcut for easy access later")
 st.text(" ")
-
-st.text("Find the .zip file in your files, double click it, and you " \
-"should see an app named 'Root Hair Analyzer' show up nearby. For convenience, feel free to move this app to your desktop " \
-"for easier future access. Double-clicking this app will open the analyzer.")
